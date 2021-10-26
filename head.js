07d8d09a4f4f366d596be14d37ad1e12a4df2d76
Youssef Nafaa <nafaayoussef@gmail.com>
2021-10-26 01:15:11 +0000

    Create head.js

---
+++ head.js
@@ -0,0 +1,23 @@
+// FUNCTION IMPLEMENTATION
+const assertEqual = function (actual, expected) {
+  if (actual === expected) {
+    console.log(`✅✅✅ Assertion Passed: ${actual}  === ${expected}`);
+  } else {
+    console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+const head = function(arr){
+  if(arr.length === 0){
+    return undefined;
+  }
+  else{
+    return arr[0];
+  }
+}
+
+assertEqual(head([0]),1);
+assertEqual(head([1]), 1);
+assertEqual(head([5, 6, 7]), 5);
+assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
+assertEqual(head([]), 1);

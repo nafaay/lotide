28d6ebc206c9a8871eadbc20083418b659fc5611
Youssef Nafaa <nafaayoussef@gmail.com>
2021-10-27 00:49:28 +0000

    Compare arrays

---
+++ assertArraysEqual.js
@@ -0,0 +1,25 @@
+const eqArrays = function (array1, array2) {
+  if (array1.length !== array2.length) {
+    return false;
+  }
+  else {
+    
+    for (let i = 0; i < array1.length; i++) {
+      if (array1[i] !== array2[i]) {
+        return false;
+      }
+    }
+    console.log("x");
+    return true;
+  }
+}
+
+const assertArraysEqual = function(array1, array2){
+  return eqArrays(array1, array2);
+}
+
+console.log(assertArraysEqual([1, 2, 3],[1, 2, 3]));
+console.log(assertArraysEqual([1, 2, 5], [1, 2, 3]));
+
+
+

083c04f0e304cc4c47996bffd88b61b9abd9eb6d
Youssef Nafaa <nafaayoussef@gmail.com>
2021-10-28 01:03:15 +0000

    add findKeyByValue.js

---
+++ findKeyByValue.js
@@ -0,0 +1,25 @@
+const assertEqual = function (actual, expected) {
+  if (actual === expected) {
+    console.log(`✅✅✅ Assertion Passed: ${actual}  === ${expected}`);
+  } else {
+    console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+const findKeyByValue = function(myObject, myValue){
+  for(key of Object.keys(myObject)){
+    if (myObject[key].trim() == myValue.trim()){
+      return key;
+    }
+  }
+  return undefined
+}
+
+const bestTVShowsByGenre = {
+  sci_fi: "The Expanse",
+  comedy: "Brooklyn Nine-Nine",
+  drama: "The Wire"
+};
+
+assertEqual(findKeyByValue(bestTVShowsByGenre, "The Wire"), "drama");
+assertEqual(findKeyByValue(bestTVShowsByGenre, "That '70s Show"), undefined);

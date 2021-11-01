f0ae0d1d361f4f8aa61d853d93a1055675aee264
Youssef Nafaa <nafaayoussef@gmail.com>
2021-11-01 13:52:56 -0400

    Use Mocha and Chai in middle.js

--- test/headTest.js
+++ test/headTest.js
@@ -1,11 +1,13 @@
-const assertEqual = require('../assertEqual');
-const head = require('../head');
-
-// TEST CODE
-assertEqual(head([0]),1);
-assertEqual(head([1]), 1);
-assertEqual(head([5, 6, 7]), 5);
-assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
-assertEqual(head([]), 1);
+const assert = require('chai').assert;
+const head   = require('../head');
 
 
+// TEST CODE using Mocha and Chai
+describe("#head", () => {
+  it("returns 1 for [1, 2, 3]", () => {
+    assert.strictEqual(head([1, 2, 3]), 1);
+  });
+  it("should return '5' for ['5']", () => {
+   assert.strictEqual(head(['5']), 5);
+  })
+});
